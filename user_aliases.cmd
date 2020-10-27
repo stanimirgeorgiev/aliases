fbfc21082f331256480d92415444c754069c9c3f
Stanimir Georgiev <s.georgiev@fio.de>
2020-10-27 10:52:53 +0200

    вт 27.10.2020 г.

--- user_aliases.cmd
+++ user_aliases.cmd
@@ -19,7 +19,7 @@ e.=explorer .
 g=git $*  
 gen=npm run gen:all  
 gl=git log --oneline --all --graph --decorate  $*
-gnb=echo Checkingout branch: $1 && git ch $1 &&  echo [41mResetting branch: $1 [0m && git rh && git del && echo Pulling from branch: $1 && git pl && echo Creating branch: $2 && git chb $2 && echo [32mSuccess !!![0m
+gnb=echo Checkingout branch: $1 && git ch $1 &&  echo [41mResetting branch: $1 [0m&& git rh && git del && echo Pulling from branch: $1 && git pl && echo Creating branch: $2 && git chb $2 && echo [32mSuccess !!![0m
 gnm=echo Checkingout branch: master && git ch master &&  echo [41mResetting branch: master[0m && git rh && git del && echo Pulling from branch: master && git pl && echo Creating branch: $1 && git chb $1 && echo [32mSuccess !!![0m
 gob=echo You should add 'set LELE=$$$11' before exit line in the cmder\vendor\init.bat && echo Checkingout master && git checkout master &&  echo [41mResetting branch: master[0m && git rh && git del && git fetch --all --prune -q && git pl && echo Branches to be removed: && echo ----------------------- [44m&& git branch -vv | awk "/: gone]/{print %LELE%}" && echo [0m-----------------------
 gr=git ll | grep -B 5 -A 5 $1
@@ -41,7 +41,6 @@ ui=npm run smoke-test:ui
 unalias=alias /d $1
 updategrbcommnad=echo You should add 'set LELE=$$$11' before exit line in the cmder\vendor\init.bat  
 vi=vim $*
-ww=npm install && npm run gen:all && npm run build:watch  
 updatelocal=cp -f .\.gitconfig %home% -v $t cp -f .\agent.cmd "%GIT_INSTALL_ROOT%\usr\bin" -v $t setx backup_alias %cd% $t echo You should add 'set LELE=$$$11' before exit line in the cmder\vendor\init.bat. Create ssh keys and place them in C:\Users\s.georgiev\.ssh in order the save command to work  
 gre=git lla $b grep -B 5 -A 5 $1  
 ll=git ll $b head -$1  
@@ -51,3 +50,5 @@ test=npm run test:all
 d=docker $*  
 xargs='xargs '  
 ld=git rev-parse --abbrev-ref HEAD $b xargs -I '{}' git diff '{}' origin/'{}'  
+ww=call ..\build-scripts\backend.build.bat $t npm install $t$t npm run gen:all $t$t npm run build:watch  
+check=git ls-files -v $b grep "S "  
